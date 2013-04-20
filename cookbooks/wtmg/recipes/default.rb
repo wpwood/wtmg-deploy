eb256b639abab7d3678b27e4a7e8d8edc0de5942
Bill Woodward <wpwood@gmail.com>
2013-04-20 10:43:24 -0400

    Decrypt mysql passwords when cooking node

--- cookbooks/wtmg/recipes/default.rb
+++ cookbooks/wtmg/recipes/default.rb
@@ -10,6 +10,12 @@ user "wpwood" do
   action :create
 end
 
+# Decrypt mysql passwords
+
+databag = Chef::EncryptedDataBagItem.load("mysql", "credentials")
+node.set['mysql']['server_debian_password'] = databag['server_debian_password']
+node.set['mysql']['server_root_password'] = databag['server_root_password']
+node.set['mysql']['server_repl_password'] = databag['server_repl_password']
 
 
 
